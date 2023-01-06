b7c0f61b2398713fc1d0b215836208192c374d6e
Yaohua Li <ssliyaohua@126.com>
2023-01-06 18:05:22 +0800

    fix pdb residue name causing VMD incorrect

--- RenameResidue.py
+++ RenameResidue.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 import argparse
+# rename the residue name in PDB from UNK to the desired new name
 
 genCode = 'UNK_C4C273'
-newName = 'SiN3'
+newName = 'SN3'  # 3 letter-number combination recommended
 parser=argparse.ArgumentParser()
-parser.add_argument('fn',type=str)
+parser.add_argument('-l','--list', nargs='+', help='<Required> Set flag', required=True)
 args = parser.parse_args()
-molCode = args.fn
+
+molCode = args._get_kwargs()[0][1][0]
+newName = args._get_kwargs()[0][1][1]
+print(molCode)
+print(newName)
 tail = '.itp'
 tail2 = '.pdb'
 
--- prepare_GMX.sh
+++ prepare_GMX.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 molCode="UNK_C4C273"
-newName="SiN3"
+newName="SN3"
 
 python CutForcefield.py $molCode
-python RenameResidue.py $molCode
+python RenameResidue.py $molCode $newName
 python WriteInp.py ${newName}
 mv ${molCode}.pdb ${newName}.pdb
 mv ${molCode}.itp ${newName}.itp
