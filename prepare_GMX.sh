f728877f5b311e13caf2660d81d7e046121e7893
Yaohua Li <ssliyaohua@126.com>
2023-01-09 11:26:33 +0800

    add run writeAllTop in bash

--- prepare_GMX.sh
+++ prepare_GMX.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-molCode="UNK_C4C273"
-newName="SN3"
+molCode="UNK_D556B1"
+newName="CH5"
 
 python CutForcefield.py $molCode
 python RenameResidue.py -l $molCode $newName
 python WriteInp.py ${newName}
+python WriteAllTop.py ${newName}
 mv ${molCode}.pdb ${newName}.pdb
 mv ${molCode}.itp ${newName}.itp
 mv ffnonbonded.itp oplsaa-modif.ff/
