3717af96e8e196e57befd6b8598649ea1244a465
Yaohua Li <ssliyaohua@126.com>
2023-01-17 17:50:51 +0800

    update gromacs.sh

--- gromacs.sh
+++ gromacs.sh
@@ -10,18 +10,13 @@
 cd $SLURM_SUBMIT_DIR
 
 module add GROMACS/2019.6-intel-2019b  
-gmx grompp  -f em.mdp  -c box.pdb -p all.top -o em.tpr -maxwarn 37
-mpiexec -v gmx_mpi mdrun -v -deffnm em
-gmx grompp  -f nvt.mdp  -c em.gro  -p all.top -o nvt.tpr -maxwarn 37
+#gmx grompp  -f em.mdp  -c box.pdb -p all.top -o em.tpr -maxwarn 57
+#mpiexec -v gmx_mpi mdrun -v -deffnm em
+gmx grompp  -f mdps_Tao/nvt.mdp  -c em.gro  -p all.top -o nvt.tpr -maxwarn 57
 mpiexec -v gmx_mpi mdrun -v -deffnm nvt
-#gmx grompp  -f npt_NU.mdp  -c nvt.gro  -p all.top -o npt.tpr -maxwarn 37
-#gmx grompp -f npt_NU2.mdp  -c npt.gro -p all.top -o npt2.tpr -maxwarn 37
+gmx grompp  -f mdps_Tao/npt.mdp  -c nvt.gro  -p all.top -o npt.tpr -maxwarn 57
+mpiexec -v gmx_mpi mdrun -v -deffnm npt
 
-#mpiexec -v gmx_mpi mdrun -v -deffnm npt2
-#gmx grompp -f npt_NU.mdp -c npt2.gro -p all.top -o npt3.tpr -maxwarn 37
-
-#mpiexec -v gmx_mpi mdrun -v -deffnm npt3
-
-#gmx grompp  -f nvt3.mdp  -c npt3.gro  -p all.top -o nvt3.tpr -maxwarn 37
-
-#mpiexec -v gmx_mpi mdrun -v -deffnm nvt3
+# production run
+gmx grompp  -f mdps_Tao/run.mdp  -c npt.gro  -p all.top -o run.tpr -maxwarn 57
+mpiexec -v gmx_mpi mdrun -v -deffnm run
