3e54e39866b653323b96bd332f68cd67f4afb2de
Yaohua Li <ssliyaohua@126.com>
2023-01-19 16:46:37 +0800

    minor things again

--- gromacs.sh
+++ gromacs.sh
@@ -12,11 +12,11 @@ cd $SLURM_SUBMIT_DIR
 module add GROMACS/2019.6-intel-2019b
 #gmx grompp  -f mdps/em.mdp  -c box.pdb -p all.top -o em.tpr -maxwarn 57
 #mpiexec -v gmx_mpi mdrun -v -deffnm em
-gmx grompp  -f mdps_Tao/nvt.mdp  -c em.gro  -p all.top -o nvt.tpr -maxwarn 57
+gmx grompp  -f ../../mdps_Tao/nvt.mdp  -c em.gro  -p all.top -o nvt.tpr -maxwarn 57
 mpiexec -v gmx_mpi mdrun -v -deffnm nvt
-gmx grompp  -f mdps_Tao/npt.mdp  -c nvt.gro  -p all.top -o npt.tpr -maxwarn 57
+gmx grompp  -f ../../mdps_Tao/npt.mdp  -c nvt.gro  -p all.top -o npt.tpr -maxwarn 57
 mpiexec -v gmx_mpi mdrun -v -deffnm npt
 
 # production run
-gmx grompp  -f mdps_Tao/run.mdp  -c npt.gro  -p all.top -o run.tpr -maxwarn 57
+gmx grompp  -f ../../mdps_Tao/run.mdp  -c npt.gro  -p all.top -o run.tpr -maxwarn 57
 mpiexec -v gmx_mpi mdrun -v -deffnm run
