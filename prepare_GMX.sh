8deb6b02e56110fa4ba399c53532f285f0e9a7f2
Yaohua Li <ssliyaohua@126.com>
2023-01-28 16:33:12 +0800

    update bash

--- prepare_GMX.sh
+++ prepare_GMX.sh
@@ -4,17 +4,17 @@ molCode="UNK_C4C273"
 newName="CN3"
 name2="li"
 name3="pf6-"
+name4="fsi-"
+name5="dfob-"
 
 
 #python CutForcefield.py $molCode
 #python RenameResidue.py -l $molCode $newName
 
-#python main.py -l $newName $name2 $name3
- # change to main.py
-python WriteInp.py -l ${newName} $name2 $name3
-python WriteAllTop.py -l ${newName} $name2 $name3
-mv ${molCode}.pdb ${newName}.pdb
-mv ${molCode}.itp ${newName}.itp
-mv ffnonbonded.itp oplsaa-modif.ff/
+python main.py -l $newName $name2 $name3 $name4 $name5
+
+#mv ${molCode}.pdb ${newName}.pdb
+#mv ${molCode}.itp ${newName}.itp
+#mv ffnonbonded.itp oplsaa-modif.ff/
 
 packmol < pack_test.inp
